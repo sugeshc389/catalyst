7ebc33868d96cf6a670c75b61d327300b7b801a3
sugesh chandra m k <sugeshc389@gmail.com>
2023-11-30 22:31:00 +0530

    fatal:Navbar design part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
    <div>
-    <h1>CATALYST</h1>
+   <Navbar/>
    </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import '../Styles/Navbar.css'
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      
+
+    <div className="container teal borderYtoX">
+      <div className='logo'>Logo</div>
+      <div >
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">Team</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+       
+      </div>
+      <div className='line'></div>
+
     </div>
+
+
   )
 }
 
